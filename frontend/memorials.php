aaebf211f00c18e084a80b6ba7ddc8e39d389304
iamhammaddevx <iamhammaddev03@gmail.com>
2026-08-18 05:19:26 +0500

    fix: navbar issue resolve

--- frontend/memorials.php
+++ frontend/memorials.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -8,7 +8,7 @@
     <title>View Memorials - IamAlwaysHere</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/style.css?v=2026081701">
+    <link rel="stylesheet" href="css/style.css?v=2026081803">
 </head>
 
 <body class="memorials-page app-page">
@@ -27,7 +27,7 @@
                             <i class="bi bi-search"></i>
                         </a>
                     </li>
-                    <li>
+                    <li class="nav-item" id="nav-memories">
                         <a href="#" class="nav-link">Memories</a>
                     </li>
                     <li class="nav-item" id="nav-home">
