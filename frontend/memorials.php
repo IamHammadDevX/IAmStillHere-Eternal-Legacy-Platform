c000b5450e9964b0355dd811df1d15f8b28a1f97
iamhammaddevx <iamhammaddev03@gmail.com>
2026-08-19 13:53:23 +0500

    fix: Navbar typography now matches Dashboard

--- frontend/memorials.php
+++ frontend/memorials.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -8,7 +8,7 @@
     <title>View Memorials - IamAlwaysHere</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/style.css?v=2026081803">
+    <link rel="stylesheet" href="css/style.css?v=2026081916">
 </head>
 
 <body class="memorials-page app-page">
